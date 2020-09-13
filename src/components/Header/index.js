f4c0e0751a60b020708d1411ee4443ed28344211
fulleb <mauro3196@gmail.com>
2020-09-13 20:01:57 -0300

    react router / custom layouts / footer component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
 import './styles.scss';
 import logo from '../../assets/placeholder-logo-2.png'
 
@@ -7,7 +9,19 @@ const Header = props => {
     <header className="header">
       <div className="wrap">
         <div className="logo">
+          <Link to="/">
           <img src={logo} alt="logo" />
+          </Link>
+        </div>
+
+        <div className="callToActions">
+          <ul>
+            <li>
+              <Link to="/registration">
+                Register
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
